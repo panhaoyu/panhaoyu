6205afbb0bc91bd51a3ad367b6263982c36c9e9e
wolfpan <haoyupan@aliyun.com>
2018-12-15 19:10:10 +0800

    Create generate.py to auto push the blog and assets.

--- generate.py
+++ generate.py
@@ -1,9 +1,9 @@
 import os
 
 os.system('pelican')
-os.system('git -C ../image1 ad')
-os.system('git -C ../image1 ci "Auto Push"')
-os.system('git -C ../image1 ph')
+os.system('git -C ../images1 ad')
+os.system('git -C ../images1 ci "Auto Push"')
+os.system('git -C ../images1 ph')
 os.system('git ad')
 os.system('git ci "Auto Push"')
 os.system('git ph')
